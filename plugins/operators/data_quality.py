20ad5ac07406c8a6d4d3d955e9f1907fed603897
mikemezadev <michaelameza@gmail.com>
2025-05-17 11:01:35 -0700

    Submission

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -11,12 +11,29 @@ class DataQualityOperator(BaseOperator):
                  # Define your operators params (with defaults) here
                  # Example:
                  # conn_id = your-connection-name
+                 redshift_conn_id = '',
+                 tests=[],
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         # Map params here
         # Example:
         # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.tests = tests
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        try:
+            postgres = PostgresHook(postgres_conn_id = self.redshift_conn_id)   
+            for test in self.tests:
+                table = test.get("table")
+                result = test.get("return")
+                
+                records = postgres.get_records(table)[0]
+                if records[0] == result:
+                    self.log.info("Data quality check passed")
+                else:
+                    self.log.info("Data quality check failed")
+            self.log.info('DataQualityOperator check is finished')
+        except Exception as e:
+              self.log.info(e)  
--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -11,12 +11,30 @@ class LoadFactOperator(BaseOperator):
                  # Define your operators params (with defaults) here
                  # Example:
                  # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 table="",
+                 sql="",
+                 truncate=True,
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
         # Map params here
         # Example:
         # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.sql = sql
+        self.table = table
+        self.truncate = truncate
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        try:
+            postgres = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+            if self.truncate:
+                self.log.info(f'Truncate table {self.table}')
+                postgres.run(f'TRUNCATE {self.table}')
+                
+            self.log.info(f'Loading fact table {self.table}')
+            postgres.run(f'INSERT INTO {self.table} {self.sql}')
+            self.log.info('LoadFactOperator is finished')
+        except Exception as e:
+            self.log.info(e)    
